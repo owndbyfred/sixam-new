fc8f344cc3590ef1fc3167b83fcdea0eee1cf617
Fred <frediek@gmx.de>
2019-06-03 21:46:45 +0200

    initial

---
+++ js/ui.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var elems = document.querySelectorAll(".sidenav");
+  var instances = M.Sidenav.init(elems, {});
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var elems = document.querySelectorAll(".slider");
+  var instances = M.Slider.init(elems, {
+    indicators: false,
+    height: 1000,
+    interval: 10000
+  });
+});
